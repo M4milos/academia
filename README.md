b1a9a15c063b719269af9e2bd16a5bccb3320be8
João Guilherme <joaoguiefernandes@gmail.com>
2022-07-28 17:24:31 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Academia
+ Projeto academia
---
+++ conf/conf.inc.php
@@ -0,0 +1,10 @@
+<?php
+    // Banco de Dados
+    define('HOST', 'localhost');  
+    define('DBNAME', '');    
+    define('USER', '');  
+    define('PASSWORD', '');
+
+    define('DRIVER', 'mysql'); 
+    define('CHARSET', 'utf8');
+?>
