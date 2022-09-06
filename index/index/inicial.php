7d97034784252e8af0365b4aad9df1fa765f1935
João Guilherme <joaoguiefernandes@gmail.com>
2022-09-06 14:10:20 -0300

    Atualização inicial

--- index/index/inicial.php
+++ index/index/inicial.php
@@ -8,15 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página inicial</title>
-    <?php
-        if(!empty($_SESSION['usuario'])){
-            echo "Logado com sucesso";
-        }else{
-            echo "Cadastrado com sucesso";
-        }
-    ?>
 </head>
 <body>
-    
 </body>
 </html>
